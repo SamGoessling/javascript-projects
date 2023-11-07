36e9d001764e8c128ed7099a1f403375511f31e7
SamGoessling <samwyz@icloud.com>
2023-11-06 20:06:05 -0600

    json fetch studio

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,44 @@
-//TODO: Add Your Code Below
+window.onload = function() {
+    const apiUrl = 'https://handlers.education.launchcode.org/static/astronauts.json';
+    function createAstronautHTML(astronaut) {
+        return `
+        <div class="astronaut">
+        <div class="bio">
+        <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+        <ul>
+        <li>Hours in space: ${astronaut.hoursInSpace}</li>
+        <li>Active: <span class="${astronaut.active ? 'active' : ''}">${astronaut.active}</span></li>
+        <li>Skills: ${astronaut.skills.join(', ')}</li>
+        </ul>
+        </div>
+        <img class="avatar" src="${astronaut.picture}">
+        </div>
+      `;
+    }
+    
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(astronauts => {
+        astronauts.sort((a, b) => b.hoursInSpace - a.hoursInSpace);
+  
+        let astronautsHTML = '';
+        let astronautCount = 0;
+        
+        astronauts.forEach(astronaut => {
+            astronautsHTML += createAstronautHTML(astronaut);
+            astronautCount++;
+        });
+        
+        document.getElementById('container').innerHTML = astronautsHTML;
+        const countElement = document.createElement('h2');
+        countElement.textContent = `Astronaut Count: ${astronautCount}`;
+        document.body.insertBefore(countElement, document.getElementById('container'));
+    })
+    
+    .catch(error => {
+        console.error('Error fetching astronauts:', error);
+        document.getElementById('container').innerHTML = '<p>Error loading astronauts. Please try again later.</p>';
+    });
+  };
+  
+  
